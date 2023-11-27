ad8f3c1928515da87460841a82c5d1fb0dbcf371
BDavid12 <bolladavid@gmail.com>
2023-11-27 18:22:07 +0100

    update

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ let isUp = false;
 let isJumping = false;
 let stop = false;
 let points = 0;
+let temp = 0;
 
 document.addEventListener('keydown', (event) => {
     if (event.key === ' ' && !isJumping && !stop) {
@@ -71,7 +72,7 @@ class MovingSpike {
     constructor(element, direction) {
         this.element = element;
         this.positionX = window.innerWidth - 50;
-        this.speed = getRandomInt(3);
+        this.speed = getRandomBtwn(2000, 5000) / 1000;
         this.direction = direction;
     }
 
@@ -82,7 +83,7 @@ class MovingSpike {
 
         if (this.positionX <= 0) {
             this.positionX = window.innerWidth - 50;
-            this.speed = getRandomInt(3);
+            this.speed = getRandomBtwn(2000, 5000) / 1000;
         }
 
         requestAnimationFrame(() => this.moveSpike());
@@ -97,34 +98,9 @@ const spike2 = new MovingSpike(document.getElementById('spike2'), -1);
 const spike3 = new MovingSpike(document.getElementById('spike3'), -1);
 const spike4 = new MovingSpike(document.getElementById('spike4'), -1);
 
-let temp = 0;
-
-if(temp == 0){
-    setTimeout(() => {
-        setTimeout(() => {
-            object1.moveObject();
-            obsticle1.style.visibility = "visible";
-        }, `${getRandomBtwn(1000, 1500)}`)
-        object2.moveObject();
-        obsticle2.style.visibility = "visible";
-        setTimeout(() => {
-            object3.moveObject();
-            obsticle3.style.visibility = "visible";
-        }, `${getRandomBtwn(500, 1000)}`)
-    }, "3000")
-    temp += 1;
-}
-else{
-    object1.moveObject();
-    object2.moveObject();
-    object3.moveObject();
-}
 
 
-spike1.moveSpike();
-spike2.moveSpike();
-spike3.moveSpike();
-spike4.moveSpike();
+
 
 function jump() {
     if (!stop) {
@@ -155,38 +131,28 @@ function jump() {
 }
 
 function checkCollision() {
-    const characterRect = character.getBoundingClientRect();
-    const topSpikeRect = topSpike.getBoundingClientRect();
-    const bottomSpikeRect = bottomSpike.getBoundingClientRect();
-    const obRect = obsticle .getBoundingClientRect();
+    
+    const obsticles = [topSpike, bottomSpike, obsticle];
 
-    if (
-        characterRect.right > topSpikeRect.left && 
-        characterRect.left < topSpikeRect.right &&
-        characterRect.bottom > topSpikeRect.top &&
-        characterRect.top < topSpikeRect.bottom
-    ) {
-        if(!stop)
-        handleCollision();
-    } 
-    else if (
-        characterRect.right > bottomSpikeRect.left && 
-        characterRect.left < bottomSpikeRect.right &&
-        characterRect.top < bottomSpikeRect.bottom &&
-        characterRect.bottom > bottomSpikeRect.top
-    ) {
-        if(!stop)
-        handleCollision();
+    for (const obsticleElement of obsticles) {
+        const characterRect = character.getBoundingClientRect();
+        const obsticleRect = obsticleElement.getBoundingClientRect();
+
+        if (
+            characterRect.right > obsticleRect.left &&
+            characterRect.left < obsticleRect.right &&
+            characterRect.bottom > obsticleRect.top &&
+            characterRect.top < obsticleRect.bottom
+        ) {
+            if (!stop) {
+                handleCollision();
+                clearInterval(checkInterval);
+                return;
+            }
+        }
     }
-    else if(
-        characterRect.right > obRect.left && 
-        characterRect.left < obRect.right &&
-        characterRect.top < obRect.bottom &&
-        characterRect.bottom > obRect.top
-    ){
-        if(!stop)
-        handleCollision();
-    }
+
+    
 }
 
 function restartTimer() {
@@ -219,7 +185,7 @@ function handleCollision() {
     object.forEach((spike) => {
         spike.style.animationPlayState = 'paused';
     })
-    gameOver.style.visibility = `visible`;
+    gameOver.style.visibility = 'visible';
     character.style.visibility = 'hidden';
     topSpike.style.visibility = 'hidden';
     bottomSpike.style.visibility = 'hidden';
@@ -229,9 +195,53 @@ function handleCollision() {
     points = 0;
 }
 
-if(!stop){
-    setInterval(checkCollision, 10);
+checkInterval = setInterval(checkCollision, 10);
+
+if(temp == 0 && !stop)
+    {
+        setTimeout(() => {
+            setTimeout(() => {
+                object1.moveObject();
+                obsticle1.style.visibility = "visible";
+            }, `${getRandomBtwn(1000, 1500)}`)
+            object2.moveObject();
+            obsticle2.style.visibility = "visible";
+            setTimeout(() => {
+                object3.moveObject();
+                obsticle3.style.visibility = "visible";
+            }, `${getRandomBtwn(500, 1000)}`)
+        }, "3000")
+        setTimeout(() => {
+            spike1.moveSpike();
+            document.getElementById("spike1").style.visibility = "visible"
+        }, `${getRandomBtwn(0, 500)}`)
+        setTimeout(() => {
+            spike2.moveSpike();
+            document.getElementById("spike2").style.visibility = "visible"
+        }, `${getRandomBtwn(1000, 1500)}`)
+        setTimeout(() => {
+            spike3.moveSpike();
+            document.getElementById("spike3").style.visibility = "visible"
+        }, `${getRandomBtwn(2000, 2500)}`)
+        setTimeout(() => {
+            spike4.moveSpike();
+            document.getElementById("spike4").style.visibility = "visible"
+        }, `${getRandomBtwn(3000, 3500)}`)
+        temp += 1;
+    }
+else if(!stop)
+{
+    object1.moveObject();
+    object2.moveObject();
+    object3.moveObject();
+    spike1.moveSpike();
+    spike2.moveSpike();
+    spike3.moveSpike();
+    spike4.moveSpike();
 }
+
+
+
 
 
 
